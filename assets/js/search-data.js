739ad84e9be7713c74fdd6a63178b3ad6ac1cf93
yanpengt06 <1308091501@qq.com>
2025-08-06 02:03:37 +0000

    Deploying to gh-pages from @ yanpengt06/yanpengt06.github.io@0f417c454fd5ab6b23482f0dc44c36a07b9eaf9d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -41,6 +41,17 @@ ninja.data = [{
             window.location.href = "/blog/2025/AI_coding/";
           
         },
+      },{id: "post-just-a-moment",
+        
+          title: 'Just a moment... <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.open("https://cameronrwolfe.substack.com/p/ai-agents", "_blank");
+          
+        },
       },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
         
           title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
